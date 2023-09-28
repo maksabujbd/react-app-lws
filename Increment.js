e34f919fa74f3bb31cce97a66210dbf715a824dd
Abdul Kader <mak.sabuj@hotmail.com>
2023-09-28 15:23:44 +0600

    intro

---
+++ Increment.js
@@ -0,0 +1,55 @@
+﻿const domContainer = document.querySelector("#root");
+
+// const myElement = React.createElement(
+//     "div", null, [
+//         React.createElement("p", null, "Inner p tag 1"),
+//         React.createElement("p", null, "Inner p tag 2")
+//     ]
+// );
+
+// const myNewElement = (
+//     <div>
+//         <h1 id="display">0</h1>
+//         <div>
+//             <button id="button">Increment +</button>
+//         </div>
+//     </div>
+// );
+
+
+const Increment = () => {
+    // const reactState = React.useState(0);
+    // console.log(reactState);
+
+
+    const [counter, setCounter] = React.useState(0);
+
+    return (
+        <div>
+            <h1 id="display">{counter}</h1>
+            <div>
+                <button id="button" onClick={() => setCounter(counter + 1)}>Increment +</button>
+            </div>
+        </div>
+    );
+}
+ReactDOM.render(
+    <div className="container">
+        <Increment/>
+        <Increment/>
+        <Increment/>
+    </div>, domContainer);
+
+// let number = 0;
+// const display = document.querySelector("#display");
+// const button = document.querySelector("#button");
+//
+// button.addEventListener("click", () => {
+//     number++;
+//     display.textContent = number.toString();
+// })
+
+
+// let p = document.createElement("p");
+// p.innerHTML = "Hello text";
+// domContainer.appendChild(p);
---
+++ script.js
@@ -0,0 +1,18 @@
+﻿let number = 0;
+let number1 = 0;
+let button =  document.querySelector("#button");
+let display = document.querySelector("#display");
+
+let button1 =  document.querySelector("#button1");
+let display1 = document.querySelector("#display1");
+
+
+button.addEventListener("click", ()=>{
+   number++;
+   display.textContent = number.toString();
+});
+
+button1.addEventListener("click", ()=>{
+   number1++;
+   display1.textContent = number1.toString();
+});
